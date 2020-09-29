47852f42b2ff1b051bc78abb7e39a709434c9551
Krzysztof Karski <krzysztof.karski@gmail.com>
2020-09-29 20:22:52 +0200

    Add transactions for better concurrency support

--- app/jobs/create_extra_issues_job.rb
+++ app/jobs/create_extra_issues_job.rb
@@ -8,6 +8,13 @@ class CreateExtraIssuesJob < ApplicationJob
   protected
 
   def create_issue(args)
-    Issue.new(args)
+    ActiveRecord::Base.transaction do
+      issue = Issue.create(args)
+
+      raise ActiveRecord::Rollback if issue.has_problems?
+    end
+
+  rescue ActiveRecord::RecordInvalid
+    Rails.logger(:error, 'There was a problem creating new issue')
   end
 end
